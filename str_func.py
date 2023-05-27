8ecd4ae71d6492f549ca8e2bd9df3d8fe426962c
Serhey <zf.41@tut.by>
2023-05-28 01:39:29 +0300

    Added def title_(str_)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def upper_(str_):
     return str_.upper()
+
+
+def title_(str_):
+    """
+     Dелает заглавными первые буквы каждого слова в строке, поступившей на вход функции.
+    :param str_:
+    :return:
+    """
+    return str_.title()
